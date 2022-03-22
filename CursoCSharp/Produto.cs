b50a33e420af3e69762062f172fc097b48f11085
Yanny Dórea Moscovits <49878332+yannymoscovits@users.noreply.github.com>
2022-03-22 16:46:08 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp
+{
+    internal class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidade;
+        public int add;
+        public int remov;
+
+        public double ValorTotalEmEstoque()
+        {
+         return preco * quantidade;
+        }
+
+        public void AdicionarProdutos()
+        {
+            
+            quantidade = quantidade + add;
+        }
+
+        public void RemoverProdutos()
+        {
+            quantidade = quantidade - remov;
+        }
+    }
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+namespace CursoCSharp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p1 = new Produto();
+
+            Console.WriteLine("Insira o nome do produto: ");
+            p1.nome = Console.ReadLine();
+            Console.WriteLine("Insira o preço do produto: ");
+            p1.preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a quantidade do produto: ");
+            p1.quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" O nome do produto é: " + p1.nome + ",o preço : " + p1.preco.ToString("F2") +
+                " e a quantidade: " + p1.quantidade + "Total: " + p1.ValorTotalEmEstoque()) ;
+
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            p1.add = int.Parse(Console.ReadLine());
+            p1.AdicionarProdutos();
+            Console.WriteLine(" O nome do produto é: " + p1.nome + " ,o preço : " + p1.preco.ToString("F2") +
+               " a quantidade: " + p1.quantidade + " - Total: " + p1.ValorTotalEmEstoque());
+            Console.WriteLine("Digite o número a ser removido do estoque: ");
+            p1.remov = int.Parse(Console.ReadLine());
+            p1.RemoverProdutos();
+            Console.WriteLine(" O nome do produto é: " + p1.nome + " ,o preço : " + p1.preco.ToString("F2") +
+               " a quantidade: " + p1.quantidade + " - Total: " + p1.ValorTotalEmEstoque());
+
+
+
+        }
+
+    }
+}
+
